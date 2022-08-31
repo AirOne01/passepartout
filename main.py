9f7c0ba214fb10d862fb91b92199bab0b21a7c3f
AirOne01 <popgthyrd@gmail.com>
2022-08-31 13:35:05 +0200

    feat: abstract window components and add title

--- main.py
+++ main.py
@@ -1,30 +1,58 @@
 #!/bin/python3
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt6.QtWidgets import *
+from PyQt6.QtGui import *
 
 from src.config import ConfigLoader
 
-class MainWindow(QMainWindow):
-  label_main: QLabel
-
+class MainWidget(QWidget):
   def __init__(self) -> None:
     super().__init__()
+    self.initUI()
 
+  def initUI(self) -> None:
+    vbox = QGridLayout()
+
+    title = TitleWidget()
+    title.setFixedHeight(70)
+    vbox.addWidget(title)
+
+    self.setLayout(vbox)
+
+class TitleWidget(QWidget):
+  def __init__(self) -> None:
+    super().__init__()
+    self.initUI()
+
+  def initUI(self) -> None:
+    label_main = QLabel()
+    label_main.setFont(QFont('Times font', 20))
+    label_main.setText("PassePartout")
+    # label_main.setFixedHeight(50)
+    label_submain = QLabel('Arial')
+    label_submain.setText("Yout multi-tool for managing bootable ISOs")
+    # label_submain.setFixedHeight(10)
+
+    vbox = QGridLayout()
+
+    vbox.addWidget(label_main)
+    vbox.addWidget(label_submain)
+
+    self.setLayout(vbox)
+
+class MainWindow(QMainWindow):
+  def __init__(self) -> None:
+    super().__init__()
+    self.initUI()
+
+  def initUI(self) -> None:
     self.setWindowTitle("PassePartout")
-    self.label_main = QLabel()
-    self.label_main.setText("Hello world")
+    self.setCentralWidget(MainWidget())
 
-    self.setCentralWidget(self.label_main)
-  
 # Note: arguments can be passed down to Qt with sys.args
 app = QApplication([])
 
 window = MainWindow()
 window.show()
 config_loader = ConfigLoader()
-
-if (config_loader.has_created_config):
-  window.label_main.setText("HAS CREATED CONFIG")
-else:
-  window.label_main.setText("CONFIG ALREADY LOADED:\n{}".format(config_loader.config.get("DEFAULT", "Version")))
 
 app.exec()
